07e9b6c52cf392fad00e5f9096acb4a7753d877f
gappeagy <appeagyeigeorgina@gmail.com>
2025-09-15 11:16:37 +0100

    move Object.freeze to immediately after declaration

--- declarations.js
+++ declarations.js
@@ -1,11 +1,15 @@
 const escapeStr = "\`\\\/\"\'"
 const arr = [4, '2']
+Object.freeze(arr)
+
 const obj = {
     str: "string",
     num: 0,
     bool: false,
     undef: undefined,
 }
+
+Object.freeze(obj)
 
 const nested = {
     arr: [4, undefined, '2'],
@@ -16,5 +20,3 @@ const nested = {
     }
 }
 Object.freeze(nested)
-Object.freeze(arr)
-Object.freeze(obj)
