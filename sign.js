b59b4a948bdb3b8e995c565681e7ff493498b3a7
gappeagy <appeagyeigeorgina@gmail.com>
2025-09-16 18:43:32 +0100

    same sign

---
+++ sign.js
@@ -0,0 +1,19 @@
+const sign = (num) => {
+    if (num > 0) {
+        return 1
+    } else if (num < 0) {
+        return -1
+    } else {
+        return 0
+    }
+}
+
+function sameSign(a, b) {
+    if (a > 0 && b > 0) {
+        return true
+    }
+
+    if (a < 0 && b < 0) {
+        return true
+    }
+}
