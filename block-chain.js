a6f92c3235424fca51846070d89b9f9ffc7cc1d1
gappeagy <appeagyeigeorgina@gmail.com>
2025-09-16 19:44:23 +0100

    blockchain

---
+++ block-chain.js
@@ -0,0 +1,18 @@
+// blockChain function
+function blockChain(data, prev = { index: 0, hash: '0' }) {
+  const index = prev.index + 1;
+  const blockData = data;
+  const hash = hashCode(index + prev.hash + JSON.stringify(blockData));
+
+  const block = {
+    index,
+    hash,
+    data: blockData,
+    prev,
+    chain(nextData) {
+      return blockChain(nextData, block);
+    }
+  };
+
+  return block;
+}
