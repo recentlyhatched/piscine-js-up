7965446c6d8aa840babf7bef2f295d357e38a537
gappeagy <appeagyeigeorgina@gmail.com>
2025-09-17 11:27:10 +0100

    handle negative numbers

--- elementary.js
+++ elementary.js
@@ -1,38 +1,67 @@
 const multiply = (a, b) => {
     let result = 0
+    let isNegative = false
 
-    for(let i = 1; i <= b; i++) {
-        result += a
+    if((a < 0 && b > 0) || (a > 0 && b < 0)) {
+        isNegative = true
+    }
+
+    for(let i = 1; i <= Math.abs(b); i++) {
+        result += Math.abs(a)
+    }
+    if(isNegative) {
+        return -result
     }
     return result
 }
 
 const divide = (a, b) => {
-    let result = a
+    let result = Math.abs(a)
+    let isNegative = false
+
+    if((a < 0 && b > 0) || (a > 0 && b < 0)) {
+        isNegative = true
+    }
+
     let count = 0
-    for(let i = 1; i <= b; i++) {
+    for(let i = 1; i <= Math.abs(b); i++) {
         if(result < 0) {
             break
         }
-        result -= b
+        result -= Math.abs(b)
         count++
     }
+
+    if(isNegative) {
+        return -result
+    }
+
     return count
 }
 
 function modulo(a, b) {
-    let result = a
+    let result = Math.abs(a)
+    let isNegative = false
 
-    for(let i = 1; i <= b; i++) {
+    if((a < 0 && b > 0) || (a > 0 && b < 0)) {
+        isNegative = true
+    }
     
-        result -= b
-        if((result-b) < 0) {
+    for(let i = 1; i <= Math.abs(b); i++) {
+    
+        result -= Math.abs(b)
+        if((result-Math.abs(b)) < 0) {
             break
         }
     }
+
+    if(isNegative) {
+        return -result
+    }
+
     return result
 }
 
-console.log(multiply(5, 4))
-console.log(divide(21, 4))
-console.log(modulo(21, 5))
+console.log(multiply(5, -4))
+console.log(divide(21, -4))
+console.log(modulo(21, -5))
