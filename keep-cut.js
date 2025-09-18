b8ed3d5bcfa12e2071c859f679dc507ae552ba6c
gappeagy <appeagyeigeorgina@gmail.com>
2025-09-18 18:43:45 +0100

    keep cut exercise

---
+++ keep-cut.js
@@ -0,0 +1,29 @@
+// remove first 2 characters
+function cutFirst(str) {
+  return str.slice(2);
+}
+
+// remove last 2 characters
+function cutLast(str) {
+  return str.slice(0, -2);
+}
+
+// remove first 2 and last 2 characters
+function cutFirstLast(str) {
+  return str.slice(2, -2);
+}
+
+// keep only the first 2 characters
+function keepFirst(str) {
+  return str.slice(0, 2);
+}
+
+// keep only the last 2 characters
+function keepLast(str) {
+  return str.slice(-2);
+}
+
+// keep first 2 and last 2 characters
+function keepFirstLast(str) {
+  return str.slice(0, 2) + str.slice(-2);
+}
