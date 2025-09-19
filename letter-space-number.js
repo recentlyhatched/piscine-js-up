7c7891b01df7f5f65b40cd6bf401c175b6a2f801
gappeagy <appeagyeigeorgina@gmail.com>
2025-09-19 16:53:39 +0100

    negative lookahead exercise

--- letter-space-number.js
+++ letter-space-number.js
@@ -1,5 +1,5 @@
 function letterSpaceNumber(str) {
   // "?!" negative lookahead to ensure pattern isn't followed by a letter
   const re = /[A-Za-z] \d(?![A-Za-z\d])/g
-  return str.match(re)
+  return str.match(re) || []
 }
