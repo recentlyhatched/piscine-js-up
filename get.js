7db9ad18d1cf33e736b59e6d9b2dc1e585aae228
gappeagy <appeagyeigeorgina@gmail.com>
2025-09-19 08:33:30 +0100

    get exercise

--- get.js
+++ get.js
@@ -3,6 +3,7 @@ function get(src, path) {
   let current = src;
 
   for (let i = 0; i < keys.length; i++) {
+    if (current == null) return undefined
     current = current[keys[i]];
   }
   return current;
