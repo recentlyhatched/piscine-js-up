959b26353ca55701ba43a036489ed75086960875
gappeagy <appeagyeigeorgina@gmail.com>
2025-09-19 08:47:07 +0100

    pyramid exercise

--- pyramid.js
+++ pyramid.js
@@ -1,13 +1,10 @@
 function pyramid(chars, height) {
-  let result = ""
+  if (height <= 0) return "";
+  const lines = [];
   for (let i = 1; i <= height; i++) {
-    // number of chars in this row
-    const width = 2 * i - 1
-    // spaces on each side
-    const spaces = " ".repeat(height - i)
-    // build the row
-    result += spaces + chars.repeat(width) + spaces
-    if (i < height) result += "\n"
+    const width = 2 * i - 1;
+    const leading = " ".repeat(height - i);
+    lines.push(leading + chars.repeat(width));
   }
-  return result
+  return lines.join("\n");
 }
