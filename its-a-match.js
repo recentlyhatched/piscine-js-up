34597766525af3e9d641896b0aaf562aa80d00e9
gappeagy <appeagyeigeorgina@gmail.com>
2025-09-19 16:34:50 +0100

    regex exercise

---
+++ its-a-match.js
@@ -0,0 +1,11 @@
+// Matches "hi" anywhere in the string
+const normal = /hi/;
+
+// Matches "hi" only at the beginning
+const begin = /^hi/;
+
+// Matches "hi" only at the end
+const end = /hi$/;
+
+// Matches exactly "hi" (and nothing else)
+const beginEnd = /^hi$/;
