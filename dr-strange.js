40ef44534d78773d2ce61079f7083bbc5abb45f7
gappeagy <appeagyeigeorgina@gmail.com>
2025-09-22 10:30:10 +0100

    dr strange exercise

--- dr-strange.js
+++ dr-strange.js
@@ -1,23 +1,25 @@
 function addWeek(date) {
   const days = [
-    "Sunday", "Monday", "Tuesday", "Wednesday",
-    "Thursday", "Friday", "Saturday"
+    "Sunday", "Monday", "Tuesday",
+    "Wednesday", "Thursday", "Friday", "Saturday"
   ];
-  
-  const dayIndex = date.getDay();
-  const dayName = days[dayIndex];
 
-  // find the "week number" starting from 0001-01-01 (Monday)
-  const start = new Date('0001-01-01');
-  const diffDays = Math.floor((date - start) / (1000 * 60 * 60 * 24));
+  // Use UTC-based day so local timezone/time component doesn't shift the day
+  const dayName = days[date.getUTCDay()];
+
+  // start at 0001-01-01 (UTC)
+  const startUtc = Date.UTC(1, 0, 1);
+  const dateUtc = Date.UTC(date.getUTCFullYear(), date.getUTCMonth(), date.getUTCDate());
+
+  const msPerDay = 24 * 60 * 60 * 1000;
+  const diffDays = Math.floor((dateUtc - startUtc) / msPerDay);
   const weekNumber = Math.floor(diffDays / 7) + 1;
 
-  if (weekNumber === 1) {
-    return dayName;
-  } else {
-    return "second" + dayName;
-  }
+  // Only the second week gets the "second" prefix
+  if (weekNumber === 2) return "second" + dayName;
+  return dayName;
 }
+
 
 function timeTravel({ date, hour, minute, second }) {
   const newDate = new Date(date.getTime()); // clone
