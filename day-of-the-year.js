b745e495ff5fa52b6f479de822685ac6f78b8f7c
gappeagy <appeagyeigeorgina@gmail.com>
2025-09-23 13:58:15 +0100

    day of the year exercise

--- day-of-the-year.js
+++ day-of-the-year.js
@@ -1,6 +1,12 @@
 function dayOfTheYear(date) {
-    const startOfYear = new Date(date.getFullYear(), 0, 1); // jan 1 of that year
-    const diff = date - startOfYear; // difference in milliseconds
-    const oneDay = 1000 * 60 * 60 * 24; // milliseconds in a day
-    return Math.floor(diff / oneDay) + 1; // +1 because Jan 1 is day 1
+    let days = 1;
+    while (!isFirstofFirst(date)) {
+        date.setDate(date.getDate() - 1);
+        days++;
+    }
+    return days;
+}
+
+function isFirstofFirst(date) {
+    return date.getDate() === 1 && date.getMonth() === 0;
 }
