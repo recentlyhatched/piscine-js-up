440a90730a9417f6f635aaac67519e096133c2f6
gappeagy <appeagyeigeorgina@gmail.com>
2025-09-24 12:19:06 +0100

    filter exercise

---
+++ filter.js
@@ -0,0 +1,34 @@
+function filter(arr, callback) {
+  const result;
+  for (let i = 0; i < arr.length; i++) {
+    if (callback(arr[i], i, arr)) {
+      result.push(arr[i]);
+    }
+  }
+  return result;
+}
+
+function reject(arr, callback) {
+  const result;
+  for (let i = 0; i < arr.length; i++) {
+    if (!callback(arr[i], i, arr)) {
+      result.push(arr[i]);
+    }
+  }
+  return result;
+}
+
+function partition(arr, callback) {
+  const truthy;
+  const falsy;
+  
+  for (let i = 0; i < arr.length; i++) {
+    if (callback(arr[i], i, arr)) {
+      truthy.push(arr[i]);
+    } else {
+      falsy.push(arr[i]);
+    }
+  }
+  
+  return [truthy, falsy];
+}
