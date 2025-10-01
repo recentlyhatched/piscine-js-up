a83f99f89ef05dbbd1640df5c7f1f040b3d1f57c
gappeagy <appeagyeigeorgina@gmail.com>
2025-10-01 14:08:38 +0100

    all exercise

---
+++ all.js
@@ -0,0 +1,8 @@
+async function all(obj) {
+  const entries = Object.entries(obj); // [[key, value], ...]
+  const results = await Promise.all(
+    entries.map(([_, value]) => Promise.resolve(value))
+  );
+  // Rebuild object with the same keys
+  return Object.fromEntries(entries.map(([key], i) => [key, results[i]]));
+}
