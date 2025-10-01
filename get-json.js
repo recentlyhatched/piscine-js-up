7f3ff2a5a0539ef175754d7e9400fe8b00b1a65b
gappeagy <appeagyeigeorgina@gmail.com>
2025-10-01 13:10:19 +0100

    get json exercise

--- get-json.js
+++ get-json.js
@@ -1,18 +1,29 @@
-function getJSON(path, ...params) {
-    if(!params) {
-        params = []
-    }
+async function getJSON(path, params = {}) {
+  // Construct URL with optional query parameters
+  const url = new URL(path, typeof window !== 'undefined' ? window.location.origin : 'http://localhost');
+  
+  // Append query params if provided
+  Object.entries(params).forEach(([key, value]) => {
+    url.searchParams.append(key, value);
+  });
 
-    for(let i = 0; i < params.length; i++) {
-        param[i] = `${params[i]}`
-        path += params[i]
-    }
+  // Fetch the URL
+  const response = await fetch(url.toString());
 
-    fetch(path)
-     .then(response => response.json())
-     .then(data => {
-        return data})
-     .catch(err => {
-        throw new Error(err)
-     })
+  // Throw if response status is not OK
+  if (!response.ok) {
+    throw new Error(response.statusText);
+  }
+
+  // Parse JSON
+  const json = await response.json();
+
+  // Return data or throw error from JSON body
+  if ('data' in json) {
+    return json.data;
+  } else if ('error' in json) {
+    throw new Error(json.error);
+  } else {
+    throw new Error('Invalid response format');
+  }
 }
